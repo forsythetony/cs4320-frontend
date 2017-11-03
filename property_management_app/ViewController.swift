e8450f465f0b9729dea92915cef5139bfa62f2b2
Tony Forsythe <forsythetony@gmail.com>
2017-11-02 20:33:24 -0500

    Initial Commit

---
+++ property_management_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  property_management_app
+//
+//  Created by Tony Forsythe on 11/2/17.
+//  Copyright © 2017 Tony Forsythe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
